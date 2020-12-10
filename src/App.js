847f93af407825db019c8a0444b01cf11a31e271
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-12-11 00:25:03 +0800

    Implement states and integrate API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import MainDisplay from './components/MainDisplay';
+
+const url = 'https://opentdb.com/api.php?amount=10&type=multiple&encode=base64';
+
+const INITIAL_ANSWERS = [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function App() {
+  // for now, assume 10 questions
+  const [ currNumber, setNumber ] = useState(0); // 0 for Start Page, 1 - 10 questions, 11 finish page
+  const [ answers, setAnswers ] = useState(INITIAL_ANSWERS);
+  const [ questions, setQuestion ] = useState([]);
+
+  useEffect(() =>
+    axios.get(url).then(res => setQuestion([null].concat(res.data.results))), []);
+
+  console.log(questions);
+  console.log(`Currently on question number ${currNumber}`);
+  const clickHandler = event => {
+    event.preventDefault();
+    let updatedAnswers = answers.concat(); // [...answers]
+    updatedAnswers[currNumber] = event.target.value;
+    setAnswers(updatedAnswers);
+    setNumber(currNumber === 10 ? 0 : currNumber + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MainDisplay questionData={questions[currNumber]} clickHandler={clickHandler}/>
       </header>
     </div>
   );
