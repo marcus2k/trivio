f7d332cfbf9a16ffe5e3361b12ec13952bbb6c84
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-12-11 15:53:30 +0800

    Add quiz settings Ui

--- src/components/WelcomeDisplay.js
+++ src/components/WelcomeDisplay.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Children } from 'react';
 import AppButton from './AppButton';
+import { makeStyles } from '@material-ui/core/styles';
+import { FormControl, FormLabel, FormControlLabel, RadioGroup, Radio, Select, InputLabel, MenuItem } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    props: {
+      fieldset: {
+        FormControl: 'secondary',
+      }
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 240//120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+}));
 
 const WelcomeDisplay = (props) => {
-    const { clickHandler } = props;
+    const classes = useStyles();
+    const { clickHandler, categories } = props;
 
     return (
         <>
@@ -18,6 +36,44 @@ const WelcomeDisplay = (props) => {
             value={0}
             text="Take a Trivia Quiz"
             />
+            <h6>You may optionally customize any of the following!</h6>
+            <FormControl component="fieldset">
+                <FormLabel component="legend">Difficulty</FormLabel>
+                <RadioGroup defaultValue="any" row aria-label="gender" name="gender1" value={Children.value} onChange={() => alert("This feature is not supported yet :(")}>
+                    <FormControlLabel value="any" control={<Radio />} label="Any" />
+                    <FormControlLabel value="easy" control={<Radio />} label="Easy" />
+                    <FormControlLabel value="medium" control={<Radio />} label="Medium" />
+                    <FormControlLabel value="hard" control={<Radio />} label="Hard" />
+                </RadioGroup>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                <Select
+                  defaultValue={-1} // any category
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={Children.value}
+                  onChange={() => alert("This feature is not supported yet :(")}
+                >
+                  <MenuItem value={-1}>Any</MenuItem>
+                  {categories.map(c => <MenuItem value={c.id}>{c.name}</MenuItem>)}
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <InputLabel id="demo-simple-select-label"># of Questions</InputLabel>
+              <Select
+                defaultValue={10}
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={Children.value}
+                onChange={() => alert("This feature is not supported yet :(")}
+              >
+                <MenuItem value={5}>5</MenuItem>
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={15}>15</MenuItem>
+                <MenuItem value={20}>20</MenuItem>
+              </Select>
+            </FormControl>
         </>
     );
 }
