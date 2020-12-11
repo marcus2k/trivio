3f109de4fa6cf585a1587a7cfc1a320353207659
Marcus <60178730+marcus2k@users.noreply.github.com>
2020-12-11 15:54:09 +0800

    Add getCategories to API services

--- src/services/opentdb.js
+++ src/services/opentdb.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const url = 'https://opentdb.com/api.php?amount=10&type=multiple&encode=base64';
 
-const getRandomQuestions = () => axios.get(url).then(res => res.data);
+const categoriesUrl = "https://opentdb.com/api_category.php";
 
-export default { getRandomQuestions };
+const getRandomQuestions = () => axios.get(url).then(res => res.data.results);
+
+const getCategories = () => axios.get(categoriesUrl).then(res => res.data.trivia_categories)
+
+export default { getRandomQuestions, getCategories };
